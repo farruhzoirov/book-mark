d6cbe16b5b01b5821e4c4bff36bff8fcd2b03db6
farruhzoirov <fzoirov29@gmail.com>
2023-10-15 13:41:22 +0500

    Add some styles

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const elsTabsItem = document.querySelectorAll('.site-main-tabs__item');
+const elsTabsPanel = document.querySelectorAll('.site-main-tabs__card');
+const elsTabLinks = document.querySelectorAll('.js-site-main-tabs__link');
+
+function deActivateTabItems() {
+    elsTabsItem.forEach(function (elsTabsItem) {
+        elsTabsItem.classList.remove('site-main-tabs__item--active')
+    })
+}
+
+function deActiveTabPanels() {
+    elsTabsPanel.forEach(function (elTabsPanel) {
+        elTabsPanel.classList.remove('site-main-tabs__card--active')
+    })
+
+}
+
+
+elsTabLinks.forEach(function (elTablink, index) {
+    elTablink.addEventListener('click', function (evt) {
+        evt.preventDefault()
+
+        // Remove active styles
+        deActivateTabItems();
+        deActiveTabPanels();
+
+        // Add active class to parent element of tabs link
+        elTablink.parentElement.classList.add('site-main-tabs__item--active')
+        elsTabsPanel[index].classList.add('site-main-tabs__card--active')
+    })
+})
